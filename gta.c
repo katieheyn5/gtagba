15bebbe5340ac9dbb5c72d8ad1a93ea016916503
Mariel Couvillion <mcouvill@mail.umw.edu>
2023-11-28 19:31:13 -0500

    deleted extra spaces and comments

--- gta.c
+++ gta.c
@@ -27,9 +27,7 @@ volatile unsigned short* bg3_control = (volatile unsigned short*) 0x400000e;
 #define PALETTE_SIZE 256
 
 volatile unsigned long* display_control = (volatile unsigned long*) 0x4000000;
-
 volatile unsigned short* bg_palette = (volatile unsigned short*) 0x5000000;
-
 volatile unsigned short* buttons = (volatile unsigned short*) 0x04000130;
 
 volatile short* bg0_x_scroll = (unsigned short*) 0x4000010;
@@ -42,9 +40,7 @@ volatile short* bg3_x_scroll = (unsigned short*) 0x400001c;
 volatile short* bg3_y_scroll = (unsigned short*) 0x400001e;
 
 volatile unsigned short* sprite_attribute_memory = (volatile unsigned short*) 0x7000000;
-
 volatile unsigned short* sprite_image_memory = (volatile unsigned short*) 0x6010000;
-
 volatile unsigned short* sprite_palette = (volatile unsigned short*) 0x5000200;
 
 #define BUTTON_A (1 << 0)
@@ -61,15 +57,11 @@ volatile unsigned short* sprite_palette = (volatile unsigned short*) 0x5000200;
 volatile unsigned short* scanline_counter = (volatile unsigned short*) 0x4000006;
 
 #define DMA_ENABLE 0x80000000
-
-/* flags for the sizes to transfer, 16 or 32 bits */
 #define DMA_16 0x00000000
 #define DMA_32 0x04000000
 
 volatile unsigned int* dma_source = (volatile unsigned int*) 0x40000D4;
-
 volatile unsigned int* dma_destination = (volatile unsigned int*) 0x40000D8;
-
 volatile unsigned int* dma_count = (volatile unsigned int*) 0x40000DC;
 
 void memcpy16_dma(unsigned short* dest, unsigned short* source, int amount) {
@@ -108,12 +100,8 @@ enum SpriteSize {
 };
 
 struct Sprite* sprite_init(int x, int y, enum SpriteSize size,
-        int horizontal_flip, int vertical_flip, int tile_index, int priority) {
-
-    /* grab the next index */
+    int horizontal_flip, int vertical_flip, int tile_index, int priority) {
     int index = next_sprite_index++;
-
-    /* setup the bits used for each shape/size possible */
     int size_bits, shape_bits;
     switch (size) {
         case SIZE_8_8:   size_bits = 0; shape_bits = 0; break;
@@ -133,40 +121,33 @@ struct Sprite* sprite_init(int x, int y, enum SpriteSize size,
     int h = horizontal_flip ? 1 : 0;
     int v = vertical_flip ? 1 : 0;
 
-    /* set up the first attribute */
-    sprites[index].attribute0 = y |             /* y coordinate */
-        (0 << 8) |          /* rendering mode */
-        (0 << 10) |         /* gfx mode */
-        (0 << 12) |         /* mosaic */
-        (1 << 13) |         /* color mode, 0:16, 1:256 */
-        (shape_bits << 14); /* shape */
+    sprites[index].attribute0 = y |  
+        (0 << 8) |          
+        (0 << 10) |         
+        (0 << 12) |         
+        (1 << 13) |         
+        (shape_bits << 14); 
 
-    /* set up the second attribute */
-    sprites[index].attribute1 = x |             /* x coordinate */
-        (0 << 9) |          /* affine flag */
-        (h << 12) |         /* horizontal flip flag */
-        (v << 13) |         /* vertical flip flag */
-        (size_bits << 14);  /* size */
+    sprites[index].attribute1 = x |             
+        (0 << 9) |         
+        (h << 12) |         
+        (v << 13) |         
+        (size_bits << 14); 
 
-    /* setup the second attribute */
-    sprites[index].attribute2 = tile_index |   // tile index */
-        (priority << 10) | // priority */
-        (0 << 12);         // palette bank (only 16 color)*/
+    sprites[index].attribute2 = tile_index |   
+        (priority << 10) | 
+        (0 << 12);         
 
-    /* return pointer to this sprite */
     return &sprites[index];
 }
 
 void sprite_update_all() {
-    /* copy them all over */
     memcpy16_dma((unsigned short*) sprite_attribute_memory, (unsigned short*) sprites, NUM_SPRITES * 4);
 }
 
 void sprite_clear() {
-    /* clear the index counter */
     next_sprite_index = 0;
 
-    /* move all sprites offscreen to hide them */
     for(int i = 0; i < NUM_SPRITES; i++) {
         sprites[i].attribute0 = SCREEN_HEIGHT;
         sprites[i].attribute1 = SCREEN_WIDTH;
@@ -174,66 +155,44 @@ void sprite_clear() {
 }
 
 void sprite_position(struct Sprite* sprite, int x, int y) {
-    /* clear out the y coordinate */
     sprite->attribute0 &= 0xff00;
-
-    /* set the new y coordinate */
     sprite->attribute0 |= (y & 0xff);
-
-    /* clear out the x coordinate */
     sprite->attribute1 &= 0xfe00;
-
-    /* set the new x coordinate */
     sprite->attribute1 |= (x & 0x1ff);
 }
 
 void sprite_move(struct Sprite* sprite, int dx, int dy) {
-    /* get the current y coordinate */
     int y = sprite->attribute0 & 0xff;
-
-    /* get the current x coordinate */
     int x = sprite->attribute1 & 0x1ff;
-
-    /* move to the new location */
     sprite_position(sprite, x + dx, y + dy);
 }
 
 void sprite_set_horizontal_flip(struct Sprite* sprite, int horizontal_flip) {
     if (horizontal_flip) {
-        /* set the bit */
         sprite->attribute1 |= 0x1000;
     } else {
-        /* clear the bit */
         sprite->attribute1 &= 0xefff;
     }
 }
 
 void sprite_set_offset(struct Sprite* sprite, int offset) {
-    /* clear the old offset */
     sprite->attribute2 &= 0xfc00;
-
-    /* apply the new one */
     sprite->attribute2 |= (offset & 0x03ff);
 }
 
 void setup_sprite_image() {
-    /* load the palette from the image into palette memory*/
     memcpy16_dma((unsigned short*) sprite_palette, (unsigned short*) car_palette, PALETTE_SIZE);
-
-    /* load the image into sprite image memory */
     memcpy16_dma((unsigned short*) sprite_image_memory, (unsigned short*) car_data, (car_width * car_height) / 2);
 }
 
 unsigned char button_pressed(unsigned short button) {
     unsigned short pressed = *buttons & button;
-
     if (pressed == 0) {
         return 1;
     } else {
         return 0;
     }
 }
-
 
 volatile unsigned short* char_block(unsigned long block) {
     return (volatile unsigned short*) (0x6000000 + (block * 0x4000));
@@ -243,9 +202,7 @@ volatile unsigned short* screen_block(unsigned long block) {
     return (volatile unsigned short*) (0x6000000 + (block * 0x800));
 }
 
-
 void setup_background() {
-
     for (int i = 0; i < PALETTE_SIZE; i++) {
         bg_palette[i] = background_palette[i];
     }
@@ -256,21 +213,19 @@ void setup_background() {
         dest[i] = image[i];
     }
 
-    *bg0_control = 0 |    /* priority, 0 is highest, 3 is lowest */
-        (0 << 2)  |       /* the char block the image data is stored in */
-        (0 << 6)  |       /* the mosaic flag */
-        (1 << 7)  |       /* color mode, 0 is 16 colors, 1 is 256 colors */
-        (16 << 8) |       /* the screen block the tile data is stored in */
-        (1 << 13) |       /* wrapping flag */
-        (0 << 14);        /* bg size, 0 is 256x256 */
+    *bg0_control = 0 |   
+        (0 << 2)  |       
+        (0 << 6)  |       
+        (1 << 7)  |       
+        (16 << 8) |       
+        (1 << 13) |      
+        (0 << 14); 
 
     dest = screen_block(16);
     for (int i = 0; i < (gta_map_width * gta_map_height); i++) {
         dest[i] = gta_map[i];
     }
 }
-
-
 
 struct Car {
     struct Sprite* sprite;
@@ -294,30 +249,24 @@ void car_init(struct Car* car) {
 }
 
 int car_left(struct Car* car) {
-    /* face left */
     sprite_set_horizontal_flip(car->sprite, 1);
     car->move = 1;
 
-    /* if we are at the left end, just scroll the screen */
     if (car->x < car->border) {
         return 1;
     } else {
-        /* else move left */
         car->x--;
         return 0;
     }
 }
 
 int car_right(struct Car* car) {
-    /* face right */
     sprite_set_horizontal_flip(car->sprite, 0);
     car->move = 1;
 
-    /* if we are at the right end, just scroll the screen */
     if (car->x > (SCREEN_WIDTH - 16 - car->border)) {
         return 1;
     } else {
-        /* else move right */
         car->x++;
         return 0;
     }
@@ -342,7 +291,6 @@ void car_update(struct Car* car) {
             car->counter = 0;
         }
     }
-
     sprite_position(car->sprite, car->x, car->y);
 }
 
@@ -363,7 +311,6 @@ int main() {
     while (1) {
         car_update(&car);
 
-        /* now the arrow keys move the car */
         if (button_pressed(BUTTON_RIGHT)) {
             if (car_right(&car)) {
                 xscroll++;
@@ -383,4 +330,3 @@ int main() {
         delay(200);
     }
 }
-
