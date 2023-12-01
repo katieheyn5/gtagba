9ef5fd40bf97695d5aa7c12a8f818f98a10065a1
Mariel Couvillion <mcouvill@mail.umw.edu>
2023-12-01 12:31:06 -0500

    fixed assembly functions?

--- reset.s
+++ reset.s
@@ -2,10 +2,12 @@
 
 reset:
     mov r1, #1
+    mov r2, r0
 top:
-    cmp r0, #3
+    cmp r2, #3
     beq done
-    add r0, r0, r1
+    add r2, r2, r1
     b top
 done:
+    mov r0, r2
     mov pc, lr
--- subtract.s
+++ subtract.s
@@ -2,11 +2,13 @@
 
 subtract:
     mov r1, #1
-    cmp r0, #0
+    mov r2, r0
+    cmp r2, #0
     bgt sub
     b done
 sub:
-    sub r0, r0, r1
+    sub r2, r2, r1
     b done 
 done:
+    mov r0, r2
     mov pc, lr
