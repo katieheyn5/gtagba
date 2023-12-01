f45ac2ef4446b64af1bac9cd42bb7f2c49693f8d
Katie Heyn <kheyn@mail.umw.edu>
2023-12-01 17:46:24 -0500

    tried to fix subtract but it didn't work

--- subtract.s
+++ subtract.s
@@ -2,7 +2,7 @@
 
 subtract:
     mov r1, #1
-    mov r2, r0
+    ldr r2, [r0]
     cmp r2, #0
     bgt sub
     b done
@@ -10,5 +10,5 @@ sub:
     sub r2, r2, r1
     b done 
 done:
-    mov r0, r2
+    str r0, [r2]
     mov pc, lr
